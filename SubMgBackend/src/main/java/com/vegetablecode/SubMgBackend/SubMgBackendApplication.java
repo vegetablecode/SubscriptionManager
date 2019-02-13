99f65d5df1deabfd3fb7aba9e475ad2f571cad47
Kamil <kamil4525@gmail.com>
2019-02-13 14:33:36 +0100

    Spring Boot Initial Project
    imported: DevTools, Web, JPA, H2, MySQL

---
+++ SubMgBackend/src/main/java/com/vegetablecode/SubMgBackend/SubMgBackendApplication.java
@@ -0,0 +1,14 @@
+package com.vegetablecode.SubMgBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubMgBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubMgBackendApplication.class, args);
+	}
+
+}
+
