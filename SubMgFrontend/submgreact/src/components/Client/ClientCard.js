1fce02b4b9bd6b86c9752781c2101ddd35436c09
Kamil <kamil4525@gmail.com>
2019-02-26 16:32:39 +0100

    client card styling

--- SubMgFrontend/submgreact/src/components/Client/ClientCard.js
+++ SubMgFrontend/submgreact/src/components/Client/ClientCard.js
@@ -5,27 +5,33 @@ class ClientCard extends Component {
     const { client } = this.props;
     return (
       <tr>
-        <td>
-          <div className="form-check">{client.name}</div>
+        <td style={{ width: "100%" }}>
+          <div>
+            <b>{client.name}</b>
+          </div>
+          <div>data kolejnego rozliczenia: {client.date}</div>
+          <div>
+            informacje: <i title="stała umowa serwisowa" className="fa fa-wrench" />
+            <i title="faktura nieopłacona" className="fa fa-calendar" />
+            <i title="klient z ważnym zadaniem" className="fa fa-warning" />
+          </div>
         </td>
-        <td>Data wystawienia: {client.date}</td>
-        <td>Data czegoś tam: {client.date}</td>
         <td className="td-actions text-right">
           <button
             type="button"
             rel="tooltip"
-            title="Edit Task"
+            title="edytuj klienta"
             className="btn btn-info btn-simple btn-link"
           >
-            <i className="fa fa-edit" />
+            <i className="fa fa-eye" />
           </button>
           <button
             type="button"
             rel="tooltip"
             title="Remove"
-            className="btn btn-danger btn-simple btn-link"
+            className="btn btn-warning btn-simple btn-link"
           >
-            <i className="fa fa-times" />
+            <i className="fa fa-wrench" />
           </button>
         </td>
       </tr>
--- SubMgFrontend/submgreact/src/components/Dashboard.js
+++ SubMgFrontend/submgreact/src/components/Dashboard.js
@@ -14,8 +14,8 @@ class Dashboard extends Component {
             <div className="col-md-5">
               <div className="card  card-tasks">
                 <div className="card-header ">
-                  <h4 className="card-title">Nieopłacone faktury</h4>
-                  <p className="card-category">Faktury czekające na zapłatę</p>
+                  <h4 className="card-title">Wystawione faktury</h4>
+                  <p className="card-category">Faktury oczekujące na zapłatę</p>
                 </div>
                 <div className="card-body ">
                   <div className="table-full-width">
@@ -25,6 +25,58 @@ class Dashboard extends Component {
                       <ClientCard key={2} client={client} />
                       <ClientCard key={3} client={client} />
                       <ClientCard key={4} client={client} />
+                      </tbody>
+                    </table>
+                  </div>
+                </div>
+                <div className="card-footer ">
+                  <hr />
+                  <div className="stats">
+                    <i className="now-ui-icons loader_refresh spin" /> Zaktualizowano 3
+                    minuty temu
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-5">
+              <div className="card  card-tasks">
+                <div className="card-header ">
+                  <h4 className="card-title">Zbliżające się faktury</h4>
+                  <p className="card-category">Faktury do wystawienia w tym tygodniu</p>
+                </div>
+                <div className="card-body ">
+                  <div className="table-full-width">
+                    <table className="table">
+                      <tbody>
+                      <ClientCard key={1} client={client} />
+                      <ClientCard key={2} client={client} />
+                      <ClientCard key={3} client={client} />
+                      <ClientCard key={4} client={client} />
+                      </tbody>
+                    </table>
+                  </div>
+                </div>
+                <div className="card-footer ">
+                  <hr />
+                  <div className="stats">
+                    <i className="now-ui-icons loader_refresh spin" /> Zaktualizowano 3
+                    minuty temu
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-5">
+              <div className="card  card-tasks">
+                <div className="card-header ">
+                  <h4 className="card-title">Ważne zadania</h4>
+                  <p className="card-category">Klienci z aktywnymi ważnymi zdaniami</p>
+                </div>
+                <div className="card-body ">
+                  <div className="table-full-width">
+                    <table className="table">
+                      <tbody>
+                      <ClientCard key={1} client={client} />
+                      <ClientCard key={2} client={client} />
                       </tbody>
                     </table>
                   </div>
